1e000af187e92711f5262dd0748a773c65eec0b7
Ekaterina21200744 <e21200744@gmail.com>
2025-10-16 21:58:04 +0500

    dz6 picture

--- 06_lesson/picture.py
+++ 06_lesson/picture.py
@@ -3,15 +3,26 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
-driver.implicitly_wait(10)
+waiter = WebDriverWait(driver, 20)
+
+
 driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
 
-pictr = driver.find_element(By.CSS_SELECTOR, "#award")
-src = pictr.get_attribute("src")
+
+# images = waiter.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "img")))
+
+sleep (10)
+pictures = driver.find_elements(By.CSS_SELECTOR, "img")
+
+# l = len(pictures)
+# print(l)
+
+src = pictures[2].get_attribute("src")
 
 print(src)
 
